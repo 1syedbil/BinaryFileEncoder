113e5f8cdd1d7e6103f93f3e152ae1435a58d933
Bilal Syed <bsyed7633@conestogac.on.ca>
2025-01-30 19:05:25 -0500

    Minor change to main. Code is compiling and working.

--- src/main.c
+++ src/main.c
@@ -7,43 +7,43 @@ int main(int argc, char* argv[])
 	switch(action)
 	{
 		case ERROR_EXIT:
-		return ERROR_EXIT;
+		break;
 
 		case HELP:
 		printf("Usage message.\n");
-		return HELP;
+		break;
 
 		case FILE_TO_FILE:
 		printf("Encode specified input file to specified asm output file.\n");
-		return FILE_TO_FILE;
+		break;
 
 		case FILE_TO_SREC_FILE:
 		printf("Encode specified input file to specified srec output file.\n");
-		return FILE_TO_SREC_FILE;
+		break;
 
 		case FILE_TO_DEF_FILE:
 		printf("Encode specified input file to default asm output file.\n");
-		return FILE_TO_DEF_FILE;
+		break;
 
 		case FILE_TO_DEF_SREC_FILE:
 		printf("Encode specified input file to default srec output file.\n");
-		return FILE_TO_DEF_SREC_FILE;
+		break;
 
 		case STDIN_TO_FILE:
 		printf("Encode from standard input to specified asm output file.\n");
-		return STDIN_TO_FILE;
+		break;
 
 		case STDIN_TO_SREC_FILE:
 		printf("Encode from standard input to specified srec output file.\n");
-		return STDIN_TO_SREC_FILE;
+		break;
 
 		case STDIN_TO_STDOUT:
 		printf("Encode from standard input to standard output asm format.\n");
-		return STDIN_TO_STDOUT;
+		break;
 
 		case STDIN_TO_SREC_STDOUT:
 		printf("Encode from standard input to standard output srec format.\n");
-		return STDIN_TO_SREC_STDOUT;
+		break;
 
 		default:
 		break;
